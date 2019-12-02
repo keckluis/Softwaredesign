c5f1e744d18cb92d045938ca71bd2fa0911f839d
keckluis <keckluis@gmail.com>
2019-12-02 19:25:05 +0100

    A06

---
+++ A06_Generischer_Baum/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace A06_Generischer_Baum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var tree = new TreeNode<String>();
+
+            var root = tree.createNode("root");
+            var child1 = tree.createNode("child1");
+            var child2 = tree.createNode("child2");
+
+            root.appendChild(child1);
+            root.appendChild(child2);
+
+            var grand11 = tree.createNode("grand11");
+            var grand12 = tree.createNode("grand12");
+            var grand13 = tree.createNode("grand13");
+
+            child1.appendChild(grand11);
+            child1.appendChild(grand12);
+            child1.appendChild(grand13);
+
+            var grand21 = tree.createNode("grand21");
+            
+            child2.appendChild(grand21);
+            child1.removeChild(grand12);
+
+            root.printTree(); 
+        }
+    }
+}
--- A06_Generischer_Baum/TreeNode.cs
+++ A06_Generischer_Baum/TreeNode.cs
@@ -1,17 +1,56 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace A06_Generischer_Baum
 {
-    class TreeNode
+    class TreeNode<T>
     {
-        public TreeNode Parent;
-        public List<TreeNode> Children;
+        public T value;
+        public List<TreeNode<T>> children;
 
-        public TreeNode(TreeNode parent, List<TreeNode> children) {
+        public TreeNode()
+        {
+            this.children = new List<TreeNode<T>>();
+        }
 
-            this.Parent = parent;
-            this.Children = children;
+        public TreeNode(T value)
+        {
+            this.value = value;
+            this.children = new List<TreeNode<T>>();
+        }
+
+        public TreeNode<T> createNode(T value)
+        {
+            return new TreeNode<T>(value);
+        }
+
+        public void appendChild(TreeNode<T> child)
+        {
+            children.Add(child);
+        }
+
+        public void removeChild(TreeNode<T> child)
+        {
+            children.Remove(child);
+        }
+
+        public void printTree()
+        {
+            Console.WriteLine(this.value);
+
+            printChildren("", this);
+        }
+
+        private void printChildren(string gen, TreeNode<T> parent)
+        {
+            gen += "*";
+
+            foreach (TreeNode<T> child in parent.children)
+            {
+                Console.WriteLine(gen + child.value.ToString());
+
+                printChildren(gen, child);
+            }
         }
     }
 }

955000a7d1866e58d7155eb063060e3736a39343
keckluis <keckluis@gmail.com>
2019-12-16 18:11:47 +0100

    Tree

--- A06_Generischer_Baum/Program.cs
+++ A06_Generischer_Baum/Program.cs
@@ -9,9 +9,13 @@ namespace A06_Generischer_Baum
         {
             Console.Clear();
 
-            var tree = new TreeNode<String>("tree");
+            var tree = new Tree<string>();
 
             var root = tree.createNode("root");
+
+            root.AddListener("AppendChild", HandleAppendChild);
+            root.AddListener("AppendChild", HandleAppendChild);
+
             var child1 = tree.createNode("child1");
             var child2 = tree.createNode("child2");
 
@@ -31,14 +35,28 @@ namespace A06_Generischer_Baum
             child2.appendChild(grand21);
             child1.removeChild(grand12);
 
-            root.printTree(); 
+            tree.printTree(root); 
 
             root.ForEach(Func<string>);
+
+            foreach (var child in root)
+            {
+                Console.WriteLine(child.value);
+            }
         }
 
-        static void Func<T>(TreeNode<T> node)
+        static void Func<T>(Tree<T>.TreeNode node)
         {
             Console.Write(node.value + " | ");
+        }
+
+        static void HandleAppendChild()
+        {
+            Console.WriteLine("Child added");
+        }
+        static void HandleRemoveChild()
+        {
+            Console.WriteLine("Child removed");
         }
     }
 }
---
+++ A06_Generischer_Baum/Tree.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace A06_Generischer_Baum
+{
+    class Tree<T>
+    {
+        public TreeNode createNode(T value)
+        {
+            return new TreeNode(value);
+        }
+
+        public void printTree(TreeNode node, string indent = "")
+        {
+            string output = node.ToString();
+            Console.WriteLine(indent + output);
+            indent += "*";
+
+            foreach (var child in node.children)
+            {
+                this.printTree(child, indent);
+            }
+        }
+
+        public class TreeNode: IEnumerable<TreeNode>
+        {
+            public delegate void EventHandler();
+            public T value;
+            public TreeNode parent;
+            public List<TreeNode> children;
+            private Dictionary<string, EventHandler> listeners;
+
+            public TreeNode(T value)
+            {
+                this.value = value;
+                this.parent = null;
+                this.children = new List<TreeNode>{};
+                this.listeners = new Dictionary<string, EventHandler>{};
+            }
+
+            public void appendChild(TreeNode child)
+            {
+                if(child.parent != null)
+                    child.parent.removeChild(child);
+
+                this.children.Add(child);
+                child.parent = this;
+
+                if (listeners.ContainsKey("AppendChild"))
+                {
+                    EventHandler handler = listeners["AppendChild"];
+                    handler();
+                }
+            }
+
+            public void removeChild(TreeNode child)
+            {
+                child.parent = null;
+                this.children.Remove(child);
+
+                if (listeners.ContainsKey("RemoveChild"))
+                {
+                    EventHandler listener = listeners["RemoveChild"];
+                    listener();
+                }
+            }
+
+            override public string ToString()
+            {
+                return value.ToString();
+            }
+
+            public void ForEach(Action<TreeNode> func)
+            {
+                func(this);
+                
+                foreach (var child in children)
+                {
+                    child.ForEach(func);
+                }
+            }
+
+            public TreeNode FindRoot()
+            {
+                if (parent == null)
+                    return this;
+                else
+                    return parent.FindRoot();
+            }
+
+            public void AddListener(string listenerType, EventHandler handler)
+            {
+                if (listeners.ContainsKey(listenerType))
+                {
+                    Console.WriteLine("Added another handler to " + listenerType + ".");
+                    listeners[listenerType] += handler;
+                }
+                else
+                {
+                    Console.WriteLine("Added a first handler to " + listenerType + ".");
+                    listeners.Add(listenerType, handler);
+                }
+            }
+
+            public void RemoveListener(string type, EventHandler handler)
+            {
+                if (listeners.ContainsKey(type))
+                {
+                    if (listeners[type].Method == handler.Method)
+                    {
+                        Console.WriteLine("Removing handler from " + type + ".");
+                        listeners[type] -= handler;
+                    }                    
+                }
+            }
+
+            public IEnumerator<TreeNode> GetEnumerator()
+            {
+                yield return this;
+                foreach (var childNode in this.children)
+                    foreach(var child in childNode)
+                        yield return child; 
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
